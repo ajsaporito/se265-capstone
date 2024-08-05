44d5989a018f599231fb45f565ee07320926d889
ajsaporito <139776127+ajsaporito@users.noreply.github.com>
2024-08-05 10:58:02 -0400

    worked on navbar

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -11,6 +11,7 @@
   <link rel="stylesheet" href="assets/css/font.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="assets/js/navbar.js"></script>
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <title><?php echo isset($title) ? $title : 'Page Not Found'; ?></title>
 </head>
@@ -26,7 +27,7 @@ function activePage($page) {
 ?>
 <nav style="background-color: #3c4245;" class="navbar navbar-expand-lg navbar-dark oxygen-regular">
   <div class="container">
-    <a class="navbar-brand fs-4" href="/se265-capstone" class="navbar-brand">DevSpot
+    <a class="navbar-brand fs-4" href="/se265-capstone">DevSpot
       <img src="assets/svgs/logo.svg" alt="Logo" width="40" height="40" class="px-2">
     </a>
     <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
@@ -43,22 +44,30 @@ function activePage($page) {
       <div style="background-color: #3c4245;" class="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
         <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
           <li class="nav-item mx-2">
-            <a class="nav-link <?= activePage('/se265-capstone/jobs');?>" href="/se265-capstone/jobs">Find Jobs</a>
+            <a class="nav-link <?= activePage('/se265-capstone/jobs'); ?>" href="/se265-capstone/jobs">Find Jobs</a>
           </li>
           <li class="nav-item mx-2">
-            <a class="nav-link <?= activePage('/se265-capstone/people');?>" href="/se265-capstone/people">Find People</a>
+            <a class="nav-link <?= activePage('/se265-capstone/people'); ?>" href="/se265-capstone/people">Find People</a>
           </li>
           <li class="nav-item mx-2">
-            <a class="nav-link <?= activePage('/se265-capstone/edit-profile');?>" href="/se265-capstone/edit-profile">Edit Profile</a>
+            <a class="nav-link <?= activePage('/se265-capstone/edit-profile'); ?>" href="/se265-capstone/edit-profile">Edit Profile</a>
           </li>
         </ul>
         <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
           <form method="post" class="mx-3">
             <div class="input-group rounded-4 overflow-hidden search-container">
               <input id="searchBox" type="text" class="form-control border-0 shadow-none py-1 px-2" placeholder="Search...">
-              <button type="submit" class="border-0 shadow-none py-1 px-2">
+              <button style="background-color: #ebebeb;" id="searchBtn" type="submit" class="border-0 shadow-none py-1 px-2" onmouseover="this.style.background='#dcdcdc'" onmouseout="this.style.background='#ebebeb'">
                 <img src="assets/svgs/magnifying-glass.svg" alt="Search" width="20" height="20" class="m-1">
               </button>
+              <span id="searchBtnBorder" class="border border-2"></span>
+              <button style="background-color: #ebebeb;" type="button" class="border-0 shadow-none py-1 px-2" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onmouseover="this.style.background='#dcdcdc'" onmouseout="this.style.background='#ebebeb'">
+                <img src="assets/svgs/caret-down.svg" alt="Toggle" width="20" height="20" class="m-1">
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Search for Jobs</a>
+                <a class="dropdown-item" href="#">Search for Users</a>
+              </div>
             </div>
           </form>
           <?php if ($isLoggedIn): ?>
