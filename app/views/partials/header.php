f080e23314e2653bd71e19256ccdcc0e52816bb6
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2024-08-12 12:47:02 -0400

    added templates for dashboard

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -7,6 +7,7 @@
   <meta name="author" content="AJ Saporito, Tristen Jussaume">
   <meta name="description" content="Associates Capstone Project">
   <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/dashboard.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <title><?php echo isset($title) ? $title : 'Page Not Found'; ?></title>
